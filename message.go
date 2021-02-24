f32b00028686cedd7ef46883a05e5410bc480256
phil lisitza <phil.lisitza@twopoint.com>
2021-02-24 06:41:13 -0600

    feat: fixed the fact that NewMessage wasn't actually parsing

--- message.go
+++ message.go
@@ -4,10 +4,11 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"reflect"
 	"strings"
+
 	"golang.org/x/net/html/charset"
-	"io/ioutil"
 )
 
 // Message is an HL7 message
@@ -20,19 +21,21 @@ type Message struct {
 // NewMessage returns a new message with the v byte value
 func NewMessage(v []byte) *Message {
 	var utf8V []byte
-	if len(v)!=0 {
+	if len(v) != 0 {
 		reader, err := charset.NewReader(bytes.NewReader(v), "text/plain")
 		if err != nil {
 			return nil
 		}
 		utf8V, err = ioutil.ReadAll(reader)
-	}else {
+	} else {
 		utf8V = v
 	}
-	return &Message{
+	newMessage := &Message{
 		Value:      []rune(string(utf8V)),
 		Delimeters: *NewDelimeters(),
 	}
+	newMessage.parse()
+	return newMessage
 }
 
 func (m *Message) String() string {
@@ -157,7 +160,7 @@ func (m *Message) parse() error {
 		switch {
 		case ch == eof || (ch == endMsg && m.Delimeters.LFTermMsg):
 			//just for safety: cannot reproduce this on windows
-			safeii:=map[bool]int{true:len(m.Value), false:ii}[ii>len(m.Value)]
+			safeii := map[bool]int{true: len(m.Value), false: ii}[ii > len(m.Value)]
 			v := m.Value[i:safeii]
 			if len(v) > 4 { // seg name + field sep
 				seg := Segment{Value: v}
