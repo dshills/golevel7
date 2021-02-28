a6d1ba495b0f02758e3e0f89f88a1c135f4aa857
phil lisitza <phil.lisitza@twopoint.com>
2021-02-28 10:34:45 -0600

    feat: implemented a scanner for messages from an io.Reader, better for large sets of messages, instead of returning a slice of all of them like Decoder

--- messageScanner.go
+++ messageScanner.go
@@ -13,20 +13,25 @@ type MessageScanner struct {
 	err     error
 }
 
-func NewMessageScanner(r io.Reader) {
+// NewMessageScanner returns a new scanner that returns
+// hl7 messages from an io.Reader
+func NewMessageScanner(r io.Reader) *MessageScanner {
 	ms := &MessageScanner{
 		r: r,
 		b: bufio.NewScanner(r),
 	}
 	ms.b.Split(commons.CrLfSplit)
+	return ms
 }
 
 func (ms *MessageScanner) Scan() (gotOne bool) {
-	if ms.b.Scan() {
+	if scan := ms.b.Scan(); scan {
 		if ms.err = ms.b.Err(); ms.err != nil {
 			if ms.b.Bytes() != nil {
 				gotOne = true
 			}
+		} else {
+			gotOne = true
 		}
 		if gotOne {
 			ms.thisMsg = NewMessage(ms.b.Bytes())
