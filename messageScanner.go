56a2dcf83078b679f21a147ee84d91f2943b9773
phil lisitza <phil.lisitza@twopoint.com>
2021-02-28 10:02:20 -0600

    feat: start of functions to wrap a scanner-type interface around an io.Reader

---
+++ messageScanner.go
@@ -0,0 +1,48 @@
+package golevel7
+
+import (
+	"bufio"
+	"golevel7/commons"
+	"io"
+)
+
+type MessageScanner struct {
+	r       io.Reader
+	b       *bufio.Scanner
+	thisMsg *Message
+	err     error
+}
+
+func NewMessageScanner(r io.Reader) {
+	ms := &MessageScanner{
+		r: r,
+		b: bufio.NewScanner(r),
+	}
+	ms.b.Split(commons.CrLfSplit)
+}
+
+func (ms *MessageScanner) Scan() bool {
+	gotOne := false
+	if ms.b.Scan() {
+		if ms.err = ms.b.Err(); ms.err != nil {
+			if ms.b.Bytes() != nil {
+				gotOne = true
+			}
+		}
+		if gotOne {
+			ms.thisMsg = NewMessage(ms.b.Bytes())
+		} else {
+			ms.thisMsg = nil
+		}
+		return gotOne
+	}
+	return gotOne
+}
+
+func (ms *MessageScanner) Message() *Message {
+	return ms.thisMsg
+}
+
+func (ms *MessageScanner) Err() error {
+	return ms.err
+}
