e18d9adc99e4051de935fcba6ee39a8a7813428e
phil lisitza <phil.lisitza@twopoint.com>
2021-03-19 08:41:10 -0500

    feat: moved io.Reader close functionality back to caller

--- messageScanner.go
+++ messageScanner.go
@@ -7,7 +7,7 @@ import (
 )
 
 type MessageScanner struct {
-	r       io.ReadCloser
+	r       io.Reader
 	b       *bufio.Scanner
 	thisMsg *Message
 	err     error
@@ -15,7 +15,7 @@ type MessageScanner struct {
 
 // NewMessageScanner returns a new scanner that returns
 // hl7 messages from an io.Reader
-func NewMessageScanner(r io.ReadCloser) *MessageScanner {
+func NewMessageScanner(r io.Reader) *MessageScanner {
 	ms := &MessageScanner{
 		r: r,
 		b: commons.NewBufScanner(r),
@@ -36,10 +36,10 @@ func (ms *MessageScanner) Scan() (gotOne bool) {
 			ms.thisMsg = NewMessage(ms.b.Bytes())
 		} else {
 			ms.thisMsg = nil
-			ms.b = nil
-			ms.r.Close()
 		}
-		return gotOne
+	}
+	if !gotOne {
+		ms.b = nil
 	}
 	return gotOne
 }
